70a7a4364bcde3c4e9f497d007fed03a28d41c33
Aldo Ortiz <aldo.ortiz@outlook.com>
2020-11-29 17:53:50 -0600

    PlayPauseRequested event.

--- SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile/Pages/PlayerPage.xaml.cs
+++ SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile/Pages/PlayerPage.xaml.cs
@@ -19,6 +19,7 @@ namespace SrtVideoPlayer.Mobile.Pages
             InitializeComponent();
             _viewModel = ViewModelLocator.Instance.Resolve<PlayerViewModel>();
             BindingContext = _viewModel;
+            _viewModel.PlayPauseRequested += Player_PlayPauseRequested;
         }
 
         public override void OnKeyUp(string character) =>
@@ -70,22 +71,35 @@ namespace SrtVideoPlayer.Mobile.Pages
             });
         }
 
-        private void MediaElement_MediaEnded(object sender, EventArgs args)
+        private void Player_PlayPauseRequested(object sender, EventArgs args)
+        {
+            switch (Player.CurrentState)
+            {
+                case MediaElementState.Playing:
+                    Player.Pause();
+                    break;
+                case MediaElementState.Paused:
+                    Player.Play();
+                    break;
+            }
+        }
+
+        private void Player_MediaEnded(object sender, EventArgs args)
         {
 
         }
 
-        private void MediaElement_MediaFailed(object sender, EventArgs args)
+        private void Player_MediaFailed(object sender, EventArgs args)
         {
 
         }
 
-        private void MediaElement_MediaOpened(object sender, EventArgs args)
+        private void Player_MediaOpened(object sender, EventArgs args)
         {
 
         }
 
-        private void MediaElement_SeekCompleted(object sender, EventArgs args)
+        private void Player_SeekCompleted(object sender, EventArgs args)
         {
 
         }
