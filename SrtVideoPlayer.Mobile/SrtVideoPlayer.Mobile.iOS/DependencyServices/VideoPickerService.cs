00bb873c7be3ca474f5e464eca1440e79578aaab
Aldo Ortiz <aldo.ortiz@outlook.com>
2020-12-21 22:17:45 -0600

    Change video picker implementation for iOS.

--- SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile.iOS/DependencyServices/VideoPickerService.cs
+++ SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile.iOS/DependencyServices/VideoPickerService.cs
@@ -1,60 +1,61 @@
 ﻿using SrtVideoPlayer.Mobile.DependencyServices;
-using SrtVideoPlayer.Mobile.iOS.Services;
+using SrtVideoPlayer.Mobile.iOS.DependencyServices;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using UIKit;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(VideoPickerService))]
-namespace SrtVideoPlayer.Mobile.iOS.Services
+namespace SrtVideoPlayer.Mobile.iOS.DependencyServices
 {
     public class VideoPickerService : IVideoPickerService
     {
         private const string _videoMediaType = "public.movie";
 
-        private UIImagePickerController _videoPicker;
+        private readonly string[] _documentTypes = {
+            _videoMediaType
+        };
+
+        private UIDocumentPickerViewController _documentPicker;
         private TaskCompletionSource<string> _taskCompletionSource;
 
         public Task<string> SelectVideoAsync()
         {
-            _videoPicker = new UIImagePickerController
-            {
-                SourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum,
-                MediaTypes = new string[] { _videoMediaType }
-            };
+            _documentPicker = new UIDocumentPickerViewController(_documentTypes, UIDocumentPickerMode.Open);
 
-            _videoPicker.FinishedPickingMedia += OnVideoPickerFinishedPickingMedia;
-            _videoPicker.Canceled += OnVideoPickerCancelled;
+            _documentPicker.DidPickDocumentAtUrls += OnDocumentPickerDidPickDocumentAtUrls;
+            _documentPicker.WasCancelled += OnDocumentPickerWasCancelled;
 
             var window = UIApplication.SharedApplication.KeyWindow;
             var viewController = window.RootViewController;
-            viewController.PresentViewController(_videoPicker, true, null);
+            viewController.PresentViewController(_documentPicker, true, null);
 
             _taskCompletionSource = new TaskCompletionSource<string>();
             return _taskCompletionSource.Task;
         }
 
-        private void OnVideoPickerFinishedPickingMedia(object sender, UIImagePickerMediaPickedEventArgs args)
+        private void OnDocumentPickerDidPickDocumentAtUrls(object sender, UIDocumentPickedAtUrlsEventArgs args)
         {
-            if (args.MediaType == _videoMediaType)
-                _taskCompletionSource.SetResult(args.MediaUrl.AbsoluteString);
+            if (args.Urls.Any())
+                _taskCompletionSource.SetResult(args.Urls[0].AbsoluteString);
             else
                 _taskCompletionSource.SetResult(null);
-            _videoPicker.DismissModalViewController(true);
+            _documentPicker.DismissModalViewController(true);
             DetachHandlers();
         }
 
-        private void OnVideoPickerCancelled(object sender, EventArgs args)
+        private void OnDocumentPickerWasCancelled(object sender, EventArgs args)
         {
             _taskCompletionSource.SetResult(null);
-            _videoPicker.DismissModalViewController(true);
+            _documentPicker.DismissModalViewController(true);
             DetachHandlers();
         }
 
         private void DetachHandlers()
         {
-            _videoPicker.FinishedPickingMedia -= OnVideoPickerFinishedPickingMedia;
-            _videoPicker.Canceled -= OnVideoPickerCancelled;
+            _documentPicker.DidPickDocumentAtUrls -= OnDocumentPickerDidPickDocumentAtUrls;
+            _documentPicker.WasCancelled -= OnDocumentPickerWasCancelled;
         }
     }
 }
