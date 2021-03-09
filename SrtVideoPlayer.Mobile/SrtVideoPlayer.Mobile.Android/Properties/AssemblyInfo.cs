c0f99545a99aea20ac831902dbd13e613d1bb54f
Aldo Ortiz <aldo.ortiz@outlook.com>
2021-03-08 20:15:55 -0600

    Photo library permission.

--- SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile.Android/Properties/AssemblyInfo.cs
+++ SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile.Android/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Android.App;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -26,3 +27,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
