a17c7883eea5d7b43edf7d05c86d9a3d70428cf5
Aldo Ortiz <aldo.ortiz@outlook.com>
2022-07-05 11:15:57 -0400

    ShadowEffect default values.

--- SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile/Effects/ShadowEffect.cs
+++ SrtVideoPlayer.Mobile/SrtVideoPlayer.Mobile/Effects/ShadowEffect.cs
@@ -4,13 +4,13 @@ namespace SrtVideoPlayer.Mobile.Effects
 {
     public class ShadowEffect : RoutingEffect
     {
-        public Color Color { get; set; }
+        public Color Color { get; set; } = Color.Black;
 
-        public float DistanceX { get; set; }
+        public float DistanceX { get; set; } = 5;
 
-        public float DistanceY { get; set; }
+        public float DistanceY { get; set; } = 5;
 
-        public float Radius { get; set; }
+        public float Radius { get; set; } = 5;
 
         public ShadowEffect() : base($"{nameof(SrtVideoPlayer)}.{nameof(ShadowEffect)}")
         {
